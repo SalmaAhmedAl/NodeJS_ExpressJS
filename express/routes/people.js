7038b300c76cc33e5236e24d55c7be65104d4e4f
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-11-03 07:28:17 +0200

    apply MVC

--- express/routes/people.js
+++ express/routes/people.js
@@ -1,56 +1,18 @@
 const express = require("express");
-let {people} = require('../data');
-
 const router = express.Router();
+const {
+    getPeople,
+    createPerson,
+    updatePerson,
+    deletePerson
+} = require('../controllers/people');
 
-router.get('/', (req, res)=>{
-    res.status(200).json({success:true, data: people});
-});
+router.get('/', getPeople );
 
-router.post('/', (req, res)=>{
-    const {name} = req.body; //the middleware makes it possible now
-    if(!name){
-        return res.status(400).json({success: false, ms:'Please provide name value.'});
-    }
-    res.status(201).json({success: true, person: name});
-});
+router.post('/', createPerson);
 
+router.put('/:id', updatePerson);
 
-router.put('/:id', (req, res)=>{
-    const {id} = req.params;
-    const name = req.body;
-
-    const person = people.find((person)=>{
-        person.id === Number(id);
-    });
-
-    if(!person){
-        return res
-        .status(404)
-        .json({success: false, msg: `no person with id ${id}`});
-    }
-    const newPerson = people.map((person)=>{
-        if (person.id === Number(id)){
-            person.name = name;
-        }
-        return person;
-    });
-    res.status(200).json({success: true, data: newPerson});
-});
-
-router.delete('/:id', (req, res)=>{
-    const person = people.find((person)=>{
-        person.id === Number(req.params.id);
-    });
-    if(!person){
-        return res
-        .status(404)
-        .json({success: false, msg: `no person with id ${req.params.id}`});
-    }
-    const newPeople = people.filter((person)=>{
-        person.id !== Number(req.params.id);
-    });
-    return res.status(200).json({success: true, data: newPeople});
-});
+router.delete('/:id', deletePerson);
 
 module.exports = router;
